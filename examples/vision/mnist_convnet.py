067d5ad715cdd9268d2eb1d5170c4d97f504ad3b
Tingjun Yang <yangtj207@yahoo.com>
2022-04-04 08:13:38 -0500

    Change keras to tf.keras.

--- examples/vision/mnist_convnet.py
+++ examples/vision/mnist_convnet.py
@@ -11,8 +11,9 @@ Description: A simple convnet that achieves ~99% test accuracy on MNIST.
 """
 
 import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
+import tensorflow as tf
+#from tensorflow import keras
+#from tensorflow.keras import layers
 
 """
 ## Prepare the data
@@ -23,7 +24,7 @@ num_classes = 10
 input_shape = (28, 28, 1)
 
 # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 # Scale images to the [0, 1] range
 x_train = x_train.astype("float32") / 255
@@ -37,23 +38,23 @@ print(x_test.shape[0], "test samples")
 
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
 """
 ## Build the model
 """
 
-model = keras.Sequential(
+model = tf.keras.Sequential(
     [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(num_classes, activation="softmax"),
     ]
 )
 
