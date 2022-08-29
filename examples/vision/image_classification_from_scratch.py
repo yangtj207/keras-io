037c1bfd58aa4cbaa8fb1ee29f3779c187c20fbd
Tingjun Yang <yangtj207@yahoo.com>
2022-08-29 02:00:02 +0000

    Change keras to tf.keras.

--- examples/vision/image_classification_from_scratch.py
+++ examples/vision/image_classification_from_scratch.py
@@ -22,8 +22,8 @@ we use Keras image preprocessing layers for image standardization and data augme
 """
 
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+#from tensorflow import keras
+#from tensorflow.keras import layers
 
 """
 ## Load the data: the Cats vs Dogs dataset
@@ -133,10 +133,10 @@ helps expose the model to different aspects of the training data while slowing d
  overfitting.
 """
 
-data_augmentation = keras.Sequential(
+data_augmentation = tf.keras.Sequential(
     [
-        layers.RandomFlip("horizontal"),
-        layers.RandomRotation(0.1),
+        tf.keras.layers.RandomFlip("horizontal"),
+        tf.keras.layers.RandomRotation(0.1),
     ]
 )
 
@@ -231,45 +231,45 @@ Note that:
 
 
 def make_model(input_shape, num_classes):
-    inputs = keras.Input(shape=input_shape)
+    inputs = tf.keras.Input(shape=input_shape)
     # Image augmentation block
     x = data_augmentation(inputs)
 
     # Entry block
-    x = layers.Rescaling(1.0 / 255)(x)
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = tf.keras.layers.Rescaling(1.0 / 255)(x)
+    x = tf.keras.layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
 
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = tf.keras.layers.Conv2D(64, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
 
     previous_block_activation = x  # Set aside residual
 
     for size in [128, 256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
 
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
 
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+        x = tf.keras.layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
         # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+        residual = tf.keras.layers.Conv2D(size, 1, strides=2, padding="same")(
             previous_block_activation
         )
-        x = layers.add([x, residual])  # Add back residual
+        x = tf.keras.layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
 
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = tf.keras.layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
 
-    x = layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
     if num_classes == 2:
         activation = "sigmoid"
         units = 1
@@ -277,13 +277,13 @@ def make_model(input_shape, num_classes):
         activation = "softmax"
         units = num_classes
 
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    outputs = tf.keras.layers.Dense(units, activation=activation)(x)
+    return tf.keras.Model(inputs, outputs)
 
 
 model = make_model(input_shape=image_size + (3,), num_classes=2)
-keras.utils.plot_model(model, show_shapes=True)
+tf.keras.utils.plot_model(model, show_shapes=True)
 
 """
 ## Train the model
@@ -292,10 +292,10 @@ keras.utils.plot_model(model, show_shapes=True)
 epochs = 50
 
 callbacks = [
-    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+    tf.keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
 ]
 model.compile(
-    optimizer=keras.optimizers.Adam(1e-3),
+    optimizer=tf.keras.optimizers.Adam(1e-3),
     loss="binary_crossentropy",
     metrics=["accuracy"],
 )
@@ -316,10 +316,10 @@ We get to ~96% validation accuracy after training for 50 epochs on the full data
 Note that data augmentation and dropout are inactive at inference time.
 """
 
-img = keras.preprocessing.image.load_img(
+img = tf.keras.preprocessing.image.load_img(
     "PetImages/Cat/6779.jpg", target_size=image_size
 )
-img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)  # Create batch axis
 
 predictions = model.predict(img_array)
